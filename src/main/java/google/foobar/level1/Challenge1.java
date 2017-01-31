ae7f0874752fe9c80c62b372167ac34f3a09ac53
Andrew Manley <andrew.manley@quarksecurity.com>
2017-01-30 19:14:42 -0500

    level1: Solve the challenge for creating ids

---
+++ src/main/java/google/foobar/level1/Challenge1.java
@@ -0,0 +1,72 @@
+package google.foobar.level1;
+
+/**
+ Re-ID
+ =====
+
+ There's some unrest in the minion ranks: minions with ID numbers like "1", "42", and other "good" numbers have been lording it over the poor minions who are stuck with more boring IDs. To quell the unrest, Commander Lambda has tasked you with reassigning everyone new, random IDs based on her Completely Foolproof Scheme.
+
+ She's concatenated the prime numbers in a single long string: "2357111317192329...". Now every minion must draw a number from a hat. That number is the starting index in that string of primes, and the minion's new ID number will be the next five digits in the string. So if a minion draws "3", their ID number will be "71113".
+
+ Help the Commander assign these IDs by writing a function answer(n) which takes in the starting index n of Lambda's string of all primes, and returns the next five digits in the string. Commander Lambda has a lot of minions, so the value of n will always be between 0 and 10000.
+
+ Languages
+ =========
+
+ To provide a Python solution, edit solution.py
+ To provide a Java solution, edit solution.java
+
+ Test cases
+ ==========
+
+ Inputs:
+ (int) n = 0
+ Output:
+ (string) "23571"
+
+ Inputs:
+ (int) n = 3
+ Output:
+ (string) "71113"
+
+ Use verify [file] to test your solution and see how it does. When you are finished editing your code, use submit [file] to submit your answer. If your solution passes the test cases, it will be removed from your home folder.
+ */
+public class Challenge1 {
+	private static final int ID_LENGTH = 5;
+	private static final int MAX_N = 10_000;
+
+	private static String idString = null;
+
+	public static String answer(int n) {
+		String idList = getIdString(MAX_N + ID_LENGTH);
+		return idList.substring(n, n + 5);
+	}
+
+	public static String getIdString(int idStringLen) {
+		if (idString != null) {
+			return idString;
+		}
+		StringBuilder sb = new StringBuilder("2");
+		int i = 3;
+		while (sb.length() < idStringLen) {
+			boolean isPrime = true;
+			for (int j = 3; j <= Math.sqrt(i + 1); j++) {
+				if (i % j == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime) {
+				sb.append(i);
+			}
+			i += 2; // Evens are never prime
+		}
+		idString = sb.toString();
+		return idString;
+	}
+
+	public static void main(String[] args) {
+		assert answer(0).equals("23571");
+		assert answer(3).equals("71113");
+	}
+}
